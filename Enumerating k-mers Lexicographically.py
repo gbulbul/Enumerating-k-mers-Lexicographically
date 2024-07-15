f35b8ae3e66c8e350240d98bef14e129503a8525
Gul Bulbul <79763247+gbulbul@users.noreply.github.com>
2024-07-14 21:23:09 -0400

    Update Enumerating k-mers Lexicographically.py

--- Enumerating k-mers Lexicographically.py
+++ Enumerating k-mers Lexicographically.py
@@ -8,30 +8,31 @@ alphabet={1:'A', 2:'B', 3:'C', 4:'D', 5:'E', 6:'F', 7:'G', 8:'H', 9:'I', 10:'J',
 15:'O', 16:'P', 17:'Q', 18:'R', 19:'S', 20:'T', 21:'U', 22:'V', 23:'W', 24:'X', 25:'Y', 26:'Z'}
 new_list_keys=[]
 new_list_values=[]
-for key1 in alphabet.keys():
-    for key2 in alphabet.keys():
-       for v in range(len(list(df[0]))-1):
-            if list(df[0])[v+1]==alphabet[key1]:
-                if list(df[0])[v]==alphabet[key2]:
-                      new_list_keys.append(min(key1,key2))
-                      new_list_values.append(min(alphabet[key1],alphabet[key2]))
-                      print(new_list_keys,new_list_values)
+def get_values_(alphabet):
+    for key1 in alphabet.keys():
+        for key2 in alphabet.keys():
+           for v in range(len(list(df[0]))-1):
+                if list(df[0])[v+1]==alphabet[key1]:
+                    if list(df[0])[v]==alphabet[key2]:
+                          new_list_keys.append(min(key1,key2))
+                          new_list_values.append(min(alphabet[key1],alphabet[key2]))
+                          #print(f' new_list_keys, new_list_values: {new_list_keys},{new_list_values}')
+    return new_list_values
+                          
 ele=list(df[0])[-1]
-#print(ele)
+new_list_values=get_values_(alphabet)
 new_list_values.append(ele)
-#print(new_list_values)
 df[0]=new_list_values
 df.sort_values(0)
-#print(df)
+
 mini_dict_values=['A','C','G','T']
-def mini_dict_fun(mini_dict_values):
-    mini_dict={}
-    for i in range(len(mini_dict_values)):
-        mini_dict[i]=mini_dict_values[i]
-    return mini_dict
-#print(mini_dict_fun(mini_dict_values))
-mini_dict=mini_dict_fun(mini_dict_values)
 class second_col:
+    def mini_dict_fun(mini_dict_values):
+        mini_dict={}
+        for i in range(len(mini_dict_values)):
+            mini_dict[i]=mini_dict_values[i]
+        return mini_dict
+    #print(mini_dict_fun(mini_dict_values))
     def second_col_arrange(mini_dict,df):
         new_list_vals_second_col=[]
         for k in range(0,len(df[1]),4):
@@ -39,9 +40,9 @@ class second_col:
                 df[1][v+k]=mini_dict[v]
                 new_list_vals_second_col.append(mini_dict[v])
         return new_list_vals_second_col
-print(second_col.second_col_arrange(mini_dict,df))
+mini_dict=second_col.mini_dict_fun(mini_dict_values)
 df[1]=second_col.second_col_arrange(mini_dict,df)
-print(df)
+#print(df)
 df['']=df[0] + df[1]
 df = df.drop(columns=[0, 1])
 print(df.to_string(index=False))
