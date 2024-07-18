56d9df0c42d235e0b092756dbf7e383a882891b2
Gul Bulbul <79763247+gbulbul@users.noreply.github.com>
2024-07-18 12:46:18 -0400

    Update Enumerating k-mers Lexicographically.py

--- Enumerating k-mers Lexicographically.py
+++ Enumerating k-mers Lexicographically.py
@@ -1,32 +1,19 @@
-bases=['A', 'C', 'G', 'T'] #
-list_of_pairs = [(b1, b2) for b1 in bases for b2 in bases]
-print(list_of_pairs)
-import pandas as pd
-df=pd.DataFrame(list_of_pairs)
-#print(list(df[0]))
-alphabet={1:'A', 2:'B', 3:'C', 4:'D', 5:'E', 6:'F', 7:'G', 8:'H', 9:'I', 10:'J', 11:'K', 12:'L', 13:'M', 14:'N', 
-15:'O', 16:'P', 17:'Q', 18:'R', 19:'S', 20:'T', 21:'U', 22:'V', 23:'W', 24:'X', 25:'Y', 26:'Z'}
-new_list_keys=[]
-new_list_values=[]
-def get_values_(alphabet):
-    for key1 in alphabet.keys():
-        for key2 in alphabet.keys():
-           for v in range(len(list(df[0]))-1):
-                if list(df[0])[v+1]==alphabet[key1]:
-                    if list(df[0])[v]==alphabet[key2]:
-                          new_list_keys.append(min(key1,key2))
-                          new_list_values.append(min(alphabet[key1],alphabet[key2]))
-                          #print(f' new_list_keys, new_list_values: {new_list_keys},{new_list_values}')
-    return new_list_values
-                          
-ele=list(df[0])[-1]
-new_list_values=get_values_(alphabet)
-new_list_values.append(ele)
-df[0]=new_list_values
-df.sort_values(0)
-
-mini_dict_values=['A','C','G','T']
-class second_col:
+class obtain_pairs:
+    def obtain_df(bases):
+        list_of_pairs = [(b1, b2) for b1 in bases for b2 in bases]
+        #print(list_of_pairs)
+        df=pd.DataFrame(list_of_pairs)
+        return df
+    def get_values_(alphabet):
+        for key1 in alphabet.keys():
+            for key2 in alphabet.keys():
+               for v in range(len(list(df[0]))-1):
+                    if list(df[0])[v+1]==alphabet[key1]:
+                        if list(df[0])[v]==alphabet[key2]:
+                              new_list_keys.append(min(key1,key2))
+                              new_list_values.append(min(alphabet[key1],alphabet[key2]))
+                              #print(f' new_list_keys, new_list_values: {new_list_keys},{new_list_values}')
+        return new_list_values
     def mini_dict_fun(mini_dict_values):
         mini_dict={}
         for i in range(len(mini_dict_values)):
@@ -40,9 +27,22 @@ class second_col:
                 df[1][v+k]=mini_dict[v]
                 new_list_vals_second_col.append(mini_dict[v])
         return new_list_vals_second_col
-mini_dict=second_col.mini_dict_fun(mini_dict_values)
-df[1]=second_col.second_col_arrange(mini_dict,df)
-#print(df)
-df['']=df[0] + df[1]
-df = df.drop(columns=[0, 1])
-print(df.to_string(index=False))
+if __name__=="__main__":
+    import pandas as pd
+    bases=['A', 'C', 'G', 'T']
+    mini_dict_values=bases
+    alphabet={1:'A', 2:'B', 3:'C', 4:'D', 5:'E', 6:'F', 7:'G', 8:'H', 9:'I', 10:'J', 11:'K', 12:'L', 13:'M', 14:'N', 
+    15:'O', 16:'P', 17:'Q', 18:'R', 19:'S', 20:'T', 21:'U', 22:'V', 23:'W', 24:'X', 25:'Y', 26:'Z'}
+    new_list_keys,new_list_values=[],[]
+    df=obtain_pairs.obtain_df(bases)
+    ele=list(df[0])[-1]
+    new_list_values=obtain_pairs.get_values_(alphabet)
+    new_list_values.append(ele)
+    df[0]=new_list_values
+    df.sort_values(0)
+    mini_dict=obtain_pairs.mini_dict_fun(mini_dict_values)
+    df[1]=obtain_pairs.second_col_arrange(mini_dict,df)
+    #print(df)
+    df['']=df[0] + df[1]
+    df = df.drop(columns=[0, 1])
+    print(df.to_string(index=False))
